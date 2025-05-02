7584e06848e56def3aa8ac728cd3b863d65b2ea0
Alexi Dg <alexisdurangomez588@gmail.com>
2025-05-02 17:45:07 -0500

    "feat: add image upload functionality"

--- app/platform/page.tsx
+++ app/platform/page.tsx
@@ -30,7 +30,6 @@ export default function Platform() {
 
   return (
     <div className="Home min-h-screen flex flex-col">
-      <Header />
       <header className="bg-slate-900 text-white p-4 lg:px-8 md:p-1 fixed top-0 right-0 left-0">
         <div className="container mx-auto flex justify-between items-center">
           <div className='flex cursor-pointer' onClick={()=>changeState(<HomeComponent />)}>
