d819c7aaf8b2f54a1fba62a406da5781b6cbb7e3
Alexi_Dg <alexisdurangomez588@gmail.com>
2025-11-21 18:29:29 -0500

    feat: :fire: i have not idea

--- api/models/teacher_models.py
+++ api/models/teacher_models.py
@@ -17,3 +17,4 @@ class Teacher(Base):
     created_at = Column(DateTime, default=datetime.now(), nullable=False)
     updated_at = Column(DateTime, default=datetime.now(), onupdate=datetime.now(), nullable=False)
     posts = relationship('Post', back_populates='teacher')
+    
